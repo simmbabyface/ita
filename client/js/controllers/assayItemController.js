b0b348738325863133d6c8f7587ea277a752e8ad
simmbabyface <csxyllx@gmail.com>
2020-09-23 01:28:20 +0800

    added navigation pill at sampleitem and assayitem

--- client/js/controllers/assayItemController.js
+++ client/js/controllers/assayItemController.js
@@ -1,6 +1,11 @@
 angular.module('ITAApp')
-.controller('assayItemController', function($scope, $window, $routeParams, $cookies, $anchorScroll, jobService){
-    $window.scrollTo(0, 0);
+.controller('assayItemController', function($scope, $location, $window, $routeParams, $cookies, $anchorScroll, jobService){
+    //$window.scrollTo(0, 0);
+    $scope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll.yOffset = 100;
+        $anchorScroll();
+    }
     jobService.assay_detail($routeParams.aid).success(function(res){
         $scope.assay_detail_info = res;
     });
--- client/js/controllers/sampleItemController.js
+++ client/js/controllers/sampleItemController.js
@@ -1,6 +1,11 @@
 angular.module('ITAApp')
-.controller('sampleItemController', function($scope, $window, $routeParams, $cookies, $anchorScroll, jobService){
-    $window.scrollTo(0, 0);
+.controller('sampleItemController', function($scope, $location, $window, $routeParams, $cookies, $anchorScroll, jobService){
+    //$window.scrollTo(0, 0);
+    $scope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll.yOffset = 100;
+        $anchorScroll();
+    }
     jobService.sample_detail($routeParams.sid).success(function(res){
         $scope.sample_detail_info = res;
     });
